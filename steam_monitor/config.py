55bbc1aba2b7ac75d544540a64df15e1187ac6c8
HuiOboro <267531448+HuiOboro@users.noreply.github.com>
2026-08-21 20:29:32 +0800

    fix(steam-monitor): 胡闹厨房2 appid 739630→728880 (739630 实为 Phasmophobia)

--- steam_monitor/config.py
+++ steam_monitor/config.py
@@ -5,5 +5,5 @@ GAMES = [
     {"appid": 1091500, "name": "赛博朋克 2077"},
     {"appid": 1245620, "name": "艾尔登法环"},
     {"appid": 1462040, "name": "FF7 Remake"},
-    {"appid": 739630, "name": "胡闹厨房 2"},
+    {"appid": 728880, "name": "胡闹厨房 2"},
 ]
